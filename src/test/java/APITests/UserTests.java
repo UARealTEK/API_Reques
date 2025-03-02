ab94e603f6af4fb00e7aa5cee41cdbcf6f8e6b26
Volodymyr <uarealtek1994@gmail.com>
2025-03-02 15:19:43 +0200

    Working on PUT request

--- src/test/java/APITests/UserTests.java
+++ src/test/java/APITests/UserTests.java
@@ -15,10 +15,8 @@ import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Stream;
 
-import static base.Steps.CreateUserSteps.getAllUsers;
 import static base.Steps.CreateUserSteps.getLastCreatedUser;
 import static base.Utils.ParserHelper.getJsonAsObjectUsingGson;
 
