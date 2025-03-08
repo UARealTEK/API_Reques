0eb6ddf9039211b450d1cd3be6a88e81895c8cac
Volodymyr <uarealtek1994@gmail.com>
2025-03-08 21:21:52 +0200

    Added test for GET Request for LoggedInUser

---
+++ src/main/java/base/Objects/LoginObjects/LoginObject.java
@@ -0,0 +1,19 @@
+package base.Objects.LoginObjects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginObject {
+
+    int id;
+    String name;
+    int year;
+    String color;
+    String pantone_value;
+}
